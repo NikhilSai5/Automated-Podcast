3491431ffa515a0e70a92e3120bab19982374ab7
NikhilSai <nikhilsaimanam5@gmail.com>
2024-01-08 11:54:49 +0530

    cached implemented in openAI service

--- openAI/index.js
+++ openAI/index.js
@@ -19,11 +19,35 @@ app.use(cors());
 app.get('/summarize-text', async (req, res) => {  
   await redisClient.connect();
 
+  const resUrl = await fetch('http://localhost:5000/api/scrape', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json', },
+        });
+        
+  const fetchedURL = await resUrl.json();
+  console.log(fetchedURL.url)
+  
+  const cacheKey = `key:${fetchedURL.url}`
+
+
   const fetched_data =  await redisClient.get("raw_data")
   if (fetched_data) {
-    const summarizedData = await summarize(fetched_data);
-    console.log(summarizedData);
-    res.json(summarizedData);
+    
+    const cachedData = await redisClient.get(cacheKey)
+    if (cachedData){
+      res.send(cachedData)
+      console.log("hello")
+    }else{
+      const summarizedData = await summarize(fetched_data);
+      console.log(summarizedData);
+
+    // save the summarized data in redis 
+      redisClient.setEx(cacheKey, 3600, JSON.stringify(summarizedData))
+      res.json(summarizedData);
+    }
+
+
+    
   } else {
     console.log('Data not found in Redis');
     res.status(404).json({ error: 'Data not found in Redis' });
--- openAI/openAIApi/openAIutils.js
+++ openAI/openAIApi/openAIutils.js
@@ -2,7 +2,7 @@ const  OpenAIApi  = require("openai");
 const Configuration = require("openai")
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
