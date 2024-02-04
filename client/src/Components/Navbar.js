097f829659041c36d08b7fd9189407b3c82a0cda
NikhilSai <nikhilsaimanam5@gmail.com>
2024-02-04 15:50:37 +0530

    navbar modified for mobile

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
@@ -25,39 +26,67 @@ const Navbar = () => {
               </Link>
             </div>
 
-            <div className="flex space-x-4 text-white">
-              <Link
-                to="/summery"
-                className="hover:text-slate-200 transition ease-in-out delay-100"
-              >
-                App
-              </Link>
-              <Link
-                href="#"
-                className="hover:text-slate-200 transition ease-in-out delay-100"
-              >
-                Features
-              </Link>
-              <Link
-                href="#"
-                className="hover:text-slate-200 transition ease-in-out delay-100"
-              >
-                Contact
-              </Link>
-              <Link
-                href="#"
-                className="hover:text-slate-200 transition ease-in-out delay-100"
-              >
-                Plans
-              </Link>
-              <Link
-                href="#"
-                className="hover:text-slate-200 transition ease-in-out delay-100"
-              >
-                About Me
-              </Link>
+            <div className="flex lg:flex-row flex-row-reverse gap-2 items-center justify-center">
+              <div className="lg:flex lg:space-x-4 text-white hidden">
+                <Link
+                  to="/summery"
+                  className="hover:text-slate-200 transition ease-in-out delay-100"
+                >
+                  App
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:text-slate-200 transition ease-in-out delay-100"
+                >
+                  Features
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:text-slate-200 transition ease-in-out delay-100"
+                >
+                  Contact
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:text-slate-200 transition ease-in-out delay-100"
+                >
+                  Plans
+                </Link>
+                <Link
+                  href="#"
+                  className="hover:text-slate-200 transition ease-in-out delay-100"
+                >
+                  About Me
+                </Link>
+              </div>
+              <div className="lg:hidden ">
+                <button
+                  data-collapse-toggle="navbar-user"
+                  type="button"
+                  class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  aria-controls="navbar-user"
+                  aria-expanded="false"
+                >
+                  <span class="sr-only">Open main menu</span>
+                  <svg
+                    class="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 17 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 1h15M1 7h15M1 13h15"
+                    />
+                  </svg>
+                </button>
+              </div>
 
-              <div>
+              <div className="profile-container">
                 {isAuthenticated ? (
                   <>
                     <button
